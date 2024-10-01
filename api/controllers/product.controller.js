af1d89f7be9982e198cad17a492ceade62dfc7ff
aklesh-dev <arzunayavadu998@gmail.com>
2024-10-01 16:19:14 +0545

    feat: Add route to fetch featured products
    - Implement a new GET route for '/featured' to retrieve featured products from the product controller.
    - Added logic to fetch featured products from Redis cache.
    - If not found in cache, retrieves featured products from MongoDB.
    - Optimized performance using `.lean()` to return plain JavaScript objects.
    - Caches the fetched products in Redis for future requests.
    - Includes error handling for cache and database operations.

--- api/controllers/product.controller.js
+++ api/controllers/product.controller.js
@@ -1,9 +1,33 @@
+import { redis } from "../lib/redis.config.js";
 import Product from "../models/product.model.js";
 
 export const getAllProducts = async (req, res) => {
   try {
     const products = await Product.find({});
     res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ messsage: "Internal Server Error", error: error.messsage });    
+  }
+};
+
+export const getFeaturedProducts = async (req, res) => {
+  try {
+    let featuredProducts = await redis.get("featured_products");
+    if (featuredProducts){
+      return res.status(200).json(JSON.parse(featuredProducts));
+    }
+
+    // if not in redis, fetch from mongodb
+    // .lean() is gonna return plain javascript objects instead of mongoose documents
+    // which is good for performance
+    featuredProducts = await Product.find({ isFeatured: true }).lean();
+
+    if(!featuredProducts) {
+      return res.status(404).json({ message: "No featured products found" });
+    }
+    // set in redis for future use
+    await redis.set("featured_products", JSON.stringify(featuredProducts));
+    return res.status(200).json(featuredProducts);
   } catch (error) {
     res.status(500).json({ messsage: "Internal Server Error", error: error.messsage });    
   }
