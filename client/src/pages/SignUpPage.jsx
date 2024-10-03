2962a9dba1f49257e0baebe20c9ec97df549c707
aklesh-dev <arzunayavadu998@gmail.com>
2024-10-03 21:26:13 +0545

    feat: Design signup page with user input form -  Add animations for improved user experience using framer-motion - Integrated state management for form data using useState hook - Create Input component for reusable text input fields.

--- client/src/pages/SignUpPage.jsx
+++ client/src/pages/SignUpPage.jsx
@@ -1,8 +1,123 @@
-import React from 'react'
+import { useState } from "react";
+import { motion } from "framer-motion";
+import { ArrowRight, Loader, Lock, Mail, User, UserPlus } from "lucide-react";
+import { Link } from "react-router-dom";
+import Input from "../components/Input";
 
 function SignUpPage() {
+  const loading = false;
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: ""
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData)
+  };
+
   return (
-    <div>SignUpPage</div>
+    <section className="flex flex-col justify-between py-2 sm:px-6 lg:px-8">
+      <motion.div
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }}
+      >
+        <h2 className="mt-6 text-center text-3xl font-extrabold text-emerald-400">Create your account</h2>
+      </motion.div>
+
+      <motion.div
+        className="mt-8 sm:mx-auto sm:w-full sm:max-w-md"
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8, delay: 0.2 }}
+      >
+        <div className="bg-gray-800 py-8 px-4 shadow-sm sm:rounded-lg sm:px-10 bg-opacity-50 backdrop-filter backdrop-blur-xl">
+          <form onSubmit={handleSubmit} className="space-y-6">
+            <div>
+              <label htmlFor="name" className="block text-sm font-semibold text-gray-300">Full Name</label>
+              <Input
+                id="name"
+                icon={User}
+                type="text"
+                required
+                placeholder="Izumi Raizel"
+                value={formData.name}
+                onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+              />
+            </div>
+
+            <div>
+              <label htmlFor="email" className="block text-sm font-semibold text-gray-300">Email address</label>
+              <Input
+                id="email"
+                icon={Mail}
+                type="email"
+                required
+                placeholder="izumi@example.com"
+                value={formData.email}
+                onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+              />
+            </div>
+            <div>
+              <label htmlFor="password" className="block text-sm font-semibold text-gray-300">Password</label>
+              <Input
+                id="password"
+                icon={Lock}
+                type="password"
+                required
+                placeholder="**********"
+                value={formData.password}
+                onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+              />
+            </div>
+            <div>
+              <label htmlFor="confirmPassword" className="block text-sm font-semibold text-gray-300">Confirm Password</label>
+              <Input
+                id="confirmPassword"
+                icon={Lock}
+                type="password"
+                required
+                placeholder="**********"
+                value={formData.confirmPassword}
+                onChange={(e) => setFormData({ ...formData, confirmPassword: e.target.value })}
+              />
+            </div>
+
+            <motion.button
+              className="mt-5 w-full flex justify-center items-center py-3 px-4 bg-gradient-to-r from-green-600 to-emerald-800 text-white font-semibold rounded-lg shadow-lg hover:from-green-600 hover:to-emerald-800 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-gray-900 transition duration-200 uppercase"
+              whileHover={{ scale: 1.02 }}
+              whileTap={{ scale: 0.98 }}
+              type="submit"
+              disabled={loading}
+            >
+              {loading ? (
+                <>
+                  <Loader className="mr-2 size-5 animate-spin" aria-hidden="true" />
+                  Signing up...
+                </>
+              ) : (
+                <>
+                  <UserPlus className="mr-2 size-5" aria-hidden="true" />
+                  <span>Sign up</span>
+                </>
+              )}
+            </motion.button>
+          </form>
+        </div>
+
+        <div className="px-4 py-4 bg-transparent ">
+          <p className="text-sm text-gray-300">
+            Already have an account?{" "}
+            <Link to={'/login'} className="text-emerald-400 hover:text-emerald-500">
+              Login <ArrowRight className="size-4 inline" />
+            </Link>
+          </p>
+        </div>
+      </motion.div>
+    </section>
   )
 }
 
