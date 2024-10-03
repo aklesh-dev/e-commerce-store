4f5530cbb9f841a435cbe7b6f04d3ca2a21591f7
aklesh-dev <arzunayavadu998@gmail.com>
2024-10-03 10:05:40 +0545

    Initialize a React app boilerplate

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+
+
+function App() {
+  
+  return (
+    <main className="font-extrabold text-blue-500 font-sans underline text-3xl text-center">This is the main content of the app</main>
+  )
+};
+
+export default App;
