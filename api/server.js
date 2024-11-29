9f77b843bbe18d0780ec09802f0583340d3e88f6
aklesh-dev <arzunayavadu998@gmail.com>
2024-11-29 08:52:28 +0545

    add pre-production build

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import path from "path";
 
 // mongoDB connection
 import connectDB from './lib/db.js';
@@ -17,6 +18,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000
 
+const __dirname = path.resolve();
+
 app.use(express.json({ limit: "10mb" })); // for parsing application/json, allows us to use req.body
 app.use(cookieParser()); // for parsing cookies, allows us to use req.cookies
 
@@ -26,6 +29,14 @@ app.use('/api/cart', cartRoutes);
 app.use('/api/coupons', couponRoutes);
 app.use('/api/payments', paymentRoutes);
 app.use('/api/analytics', analyticsRoutes);
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, "client/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
